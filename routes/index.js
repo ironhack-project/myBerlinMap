0ae67b3673f5b2bcf056642e5bbc299b5aaf4d73
Juliana Caruso <juliana.caruso@gmail.com>
2020-11-23 18:48:26 +0100

    add clean map in search view

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const axios = require('axios'); 
+const axios = require('axios');
 let coordinates
 
 /* GET home page */
@@ -8,7 +8,7 @@ router.get('/', (req, res, next) => {
   // here we want to call the api
   axios.get('https://api.quandoo.com/v1/merchants?place=Berlin&radius=10&capacity=2&offset=0&limit=10000')
     .then(response => {
-      console.log(response.data.merchants);
+      // console.log(response.data.merchants);
       coordinates = response.data.merchants.map((merchant) => {
         return [
         merchant.location.coordinates.longitude,
@@ -25,5 +25,30 @@ router.get('/rawdata', (req,res,next) => {
   // console.log(coordinates);
   return res.json (coordinates)
 });
+
+
+router.get('/search', (req, res, next) => {
+  // here we want to call the api
+  axios.get('https://api.quandoo.com/v1/merchants?place=Berlin&radius=10&capacity=2&offset=0&limit=10000')
+    .then(response => {
+      // console.log(response.data.merchants);
+      coordinates = response.data.merchants.map((merchant) => {
+        return [
+        merchant.location.coordinates.longitude,
+        merchant.location.coordinates.latitude
+        ]
+        });        
+      const restaurantList = response.data.merchants;
+      res.render('search', { restaurantList })
+    })
+});
+
+// router.post('/search', (req, res, next) => {
+//   // read searched coordinates
+//   // then search api with axios
+//   // then save result to raw file
+//   // then read that file from the mapbox.js
+//   coordinates = null; // result from api here
+// });
 
 module.exports = router;

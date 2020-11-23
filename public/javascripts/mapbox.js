3f5a3e85ca51d20682bc9310ae4c548f95259935
Kara P <quetzlcoatlus@gmail.com>
2020-11-23 12:26:51 +0100

    centered map on berlin

--- public/javascripts/mapbox.js
+++ public/javascripts/mapbox.js
@@ -2,11 +2,11 @@
 
 console.log("test");
 
-// // add some arbitrary co ords
-// let coords = [
-// 	[15.32962, 48.2271],
-// 	[7.749117, 46.020714],
-// ];
+// add some arbitrary co ords
+let coords = [
+	[15.32962, 48.2271],
+	[7.749117, 46.020714],
+];
 
 
 mapboxgl.accessToken =
@@ -14,40 +14,40 @@ mapboxgl.accessToken =
 const map = new mapboxgl.Map({
     container: "map",
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [13.405, 52.52],
+    center: [13.4050, 52.5200],
     zoom: 9,
     doubleClickZoom: true,
     pitch: 30,
 });
 
-// // demo adding a pop up
-// const popup = new mapboxgl.Popup({ closeButton: true });
-// popup.addTo(map);
-// popup.setLngLat([13.405, 52.52]);
-// popup.setMaxWidth("400px");
-// popup.setHTML(
-// 	`<h2>Click here for more info!</h2> <button>see the world</button>`
-// );
+// demo adding a pop up
+const popup = new mapboxgl.Popup({ closeButton: true });
+popup.addTo(map);
+popup.setLngLat([13.405, 52.52]);
+popup.setMaxWidth("400px");
+popup.setHTML(
+	`<h2>Click here for more info!</h2> <button>see the world</button>`
+);
 
-// coords.forEach((location) => {
-// 	console.log(location);
-// 	// https://docs.mapbox.com/mapbox-gl-js/api/markers/
-// 	let marker = new mapboxgl.Marker({
-// 		scale: 1,
-// 		draggable: true,
-// 		color: "red",
-// 		rotation: 10,
-// 	});
-// 	marker.setLngLat(location);
-// 	marker.addTo(map);
-// 	// first do the console.log and then do the rest of the stuff
-// 	marker.on("dragend", (data) => {
-// 		console.log("hello? Is it data?", data.target.getLngLat());
-// 		popup.addTo(map);
-// 		popup.setLngLat(data.target.getLngLat());
-// 		popup.setMaxWidth("400px");
-// 		popup.setHTML(
-// 			`<h2>What a location</h2> <h3>location: ${data.target.getLngLat()} </h3>`
-// 		);
-// 	});
-// });
+coords.forEach((location) => {
+	console.log(location);
+	// https://docs.mapbox.com/mapbox-gl-js/api/markers/
+	let marker = new mapboxgl.Marker({
+		scale: 1,
+		draggable: true,
+		color: "red",
+		rotation: 10,
+	});
+	marker.setLngLat(location);
+	marker.addTo(map);
+	// first do the console.log and then do the rest of the stuff
+	marker.on("dragend", (data) => {
+		console.log("hello? Is it data?", data.target.getLngLat());
+		popup.addTo(map);
+		popup.setLngLat(data.target.getLngLat());
+		popup.setMaxWidth("400px");
+		popup.setHTML(
+			`<h2>What a location</h2> <h3>location: ${data.target.getLngLat()} </h3>`
+		);
+	});
+});

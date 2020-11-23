d60103450b1d2ce8fa0b9a4ea411e4f9a6f00808
Agustina Garro <agustinagarro97@gmail.com>
2020-11-23 18:01:12 +0100

    map restrict

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,13 @@ router.get('/', (req, res, next) => {
   // here we want to call the api
   axios.get('https://api.quandoo.com/v1/merchants?place=Berlin&radius=10&capacity=2&offset=0&limit=10000')
     .then(response => {
-      // console.log(response.data.merchants);
-      const coordinates = response.data.merchants.map ((merchant) => {
-          let lon = merchant.location.coordinates.longitude;
-          let lat = merchant.location.coordinates.latitude;
-          return [lon,lat];
-    
-      });
-      console.log(coordinates);
-      //const latitude = response.data.merchants.location.coordinates.latitude;
-      //const longitude = response.data.merchants.location.coordinates.longitude;
+      console.log(response.data.merchants);
+      coordinates = response.data.merchants.map((merchant) => {
+        return [
+        merchant.location.coordinates.longitude,
+        merchant.location.coordinates.latitude
+        ]
+        });        
       const restaurantList = response.data.merchants;
       res.render('index', { restaurantList })
     })

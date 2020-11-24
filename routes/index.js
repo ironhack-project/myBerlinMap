4037f564eccaa12597cb9510f4537fe69c965a92
Agustina Garro <agustinagarro97@gmail.com>
2020-11-24 11:59:55 +0100

    login,signup

--- routes/index.js
+++ routes/index.js
@@ -26,4 +26,20 @@ router.get('/rawdata', (req,res,next) => {
   return res.json (coordinates)
 });
 
+router.get('/search', (req, res, next) => {
+  // here we want to call the api
+  axios.get('https://api.quandoo.com/v1/merchants?place=Berlin&radius=10&capacity=2&offset=0&limit=10000')
+    .then(response => {
+      // console.log(response.data.merchants);
+      coordinates = response.data.merchants.map((merchant) => {
+        return [
+        merchant.location.coordinates.longitude,
+        merchant.location.coordinates.latitude
+        ]
+        });        
+      const restaurantList = response.data.merchants;
+      res.render('search', { restaurantList })
+    })
+});
+
 module.exports = router;

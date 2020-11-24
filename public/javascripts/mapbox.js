62f35c9a40d79828c2772d12c40462c40d42fb1e
Juliana Caruso <juliana.caruso@gmail.com>
2020-11-24 16:49:28 +0100

    add dropped pin search with range

--- public/javascripts/mapbox.js
+++ public/javascripts/mapbox.js
@@ -1,8 +1,9 @@
 var userCoordinates = document.getElementById('userCoordinates');
 const popup = new mapboxgl.Popup({ closeButton: true });
+let map = null; // used for new map in the if statement (new url)
 
-let map = null;
 let berlinCenter = [13.4050, 52.5200]
+
 mapboxgl.accessToken =
 "pk.eyJ1IjoianVsaWFuYS1jYXJ1c28iLCJhIjoiY2tocWJvaGc0MHMwbDJ0cDU2cWJtd2NyNyJ9.IOr-Kg5Er1_7jZeLEIK0-A";
 
@@ -10,6 +11,10 @@ let bounds = [
 	[13.067,52.383], // SW coordinates original: 13.263522,52.416737]
 	[13.736,52.633] // NE coordinates original 13.492763,52.649208 
 ];
+
+// variable below added to be able to clear the markers 
+// after the user moves the pin to other locations
+let markers = [];
 
 if (window.location.pathname === '/') {
 	map = new mapboxgl.Map({
@@ -22,8 +27,8 @@ if (window.location.pathname === '/') {
 		maxBounds: bounds
 	});
 	
-	var nav = new mapboxgl.NavigationControl();
-	map.addControl(nav, 'top-left');
+	var nav = new mapboxgl.NavigationControl(); // is this being used?
+	map.addControl(nav, 'top-left'); // is this being used?
 
 	axios.get('/rawdataCoordinates')
 		.then(response => {
@@ -48,6 +53,7 @@ if (window.location.pathname === '/') {
 } else if (window.location.pathname === '/search') {
 	axios.get('/rawdatarestaurantNames')
 		.then(response => {
+			console.log(response);
 			let restaurantNames = response.data
 			console.log(restaurantNames)
 		});
@@ -61,8 +67,8 @@ if (window.location.pathname === '/') {
 		maxBounds: bounds
 	});
 	
-	var nav = new mapboxgl.NavigationControl();
-	map.addControl(nav, 'top-left');
+	var nav = new mapboxgl.NavigationControl(); // is this being used?
+	map.addControl(nav, 'top-left'); // is this being used?
 	
 	let userMarker = new mapboxgl.Marker({
 		scale: 1,
@@ -78,74 +84,54 @@ if (window.location.pathname === '/') {
 
 function zoomAndDisplayName (marker) {
 	marker.on("dragend", (data) => {
-		console.log(`"hello? Is it data?", ${data.target}`);
+		//console.log(`"hello? Is it data?", ${data.target.getLngLat()}`);
 		popup.addTo(map);
 		popup.setLngLat(data.target.getLngLat());
 		popup.setMaxWidth("40px");
 		// if (data.target.getLngLat() === 
-		popup.setHTML(
-			`<h3>location: ${data.target.getLngLat()} </h3>`)
+		//popup.setHTML(`<h3>location: ${data.target.getLngLat()} </h3>`);
+
+markers.forEach(marker => {
+	marker.remove();
+});
+		// gettinf=g coordinates of dropped pin for api call
+		const droppedPin = data.target.getLngLat();
 		
+		// adding text to our Div
+		const listingsDiv = document.getElementById('listings');
+
+		// new api call getting the restaurants according to droppedPin range
+		axios.get(`https://api.quandoo.com/v1/merchants?centerPoint=${droppedPin.lat}%2C%20${droppedPin.lng}&radius=1&capacity=2&offset=0&limit=10`)
+    .then(response => {
+
+			response.data.merchants.forEach((merchant) => {
+					let newMarker = new mapboxgl.Marker({
+						scale: 1,
+						draggable: false,
+						color: "green",
+						rotation: 10,
+					});
+					newMarker.setLngLat([merchant.location.coordinates.longitude, merchant.location.coordinates.latitude]);
+					newMarker.addTo(map);
+
+					// clear the pins when the user drop it to another location
+					markers.push(newMarker);
+
+					// adding text to our Div
+					listingsDiv.innerHTML += `${merchant.name}`;
+				
+				});
+
+    });
+
 		map.jumpTo({
 			center: data.target.getLngLat(),
 			zoom: 15
 		})
-		
-		axios.get('/rawdataCoordinates')
-			.then(response => {
-				// console.log(restaurantList.name)
-				let coordinates = response.data
-				
-				coordinates.forEach((location) => {
-			// console.log(location);
-			// https://docs.mapbox.com/mapbox-gl-js/api/markers/
-				let newMarker = new mapboxgl.Marker({
-					scale: 1,
-					draggable: false,
-					color: "purple",
-					rotation: 10,
-				});
-				newMarker.setLngLat(location);
-				newMarker.addTo(map);
-			});
-		});
 
-		// buildLocationList(restaurantList) //correct file needs to be added
 	});
 }
 
-// .features.forEach(function(restaurant, i){
-	// 	restaurant.id = i;
-	// });
-// function compareCoordinates (markerLocation, restaurantLocation) {
-// 	let restaurantLocationX =
-// 	let restaurantLocationY =
-// 	if (restaurantLocation.x > 10 && restaurantLocation.x < 20 && y > 10 && y < 20 ) {
-// 		return true
-// 	}
-// }
-
-
-// restaurantList.features.forEach(function(restaurant, i){
-// 	restaurant.id = i;
-// });
-
-// map.on('load', function (e) {
-// 	/* Add the data to your map as a layer */
-// 	map.addLayer({
-// 	  "id": "locations",
-// 	  "type": "symbol",
-// 	  /* Add a GeoJSON source containing place coordinates and information. */
-// 	  "source": {
-// 		"type": "geojson",
-// 		"data": restaurantList
-// 	  },
-// 	  "layout": {
-// 		"icon-image": "restaurant-15",
-// 		"icon-allow-overlap": true,
-// 	  }
-// 	});
-//   });
 
 function buildLocationList(data) {
 	data.features.forEach( (store, i) => {

57a1915e11e5d733da7d7f03f7a3e38549737bb4
Kara P <quetzlcoatlus@gmail.com>
2020-11-24 11:17:08 +0100

    juliana's multimap add

--- public/javascripts/mapbox.js
+++ public/javascripts/mapbox.js
@@ -1,47 +1,50 @@
+let map = null;
 var userCoordinates = document.getElementById('userCoordinates');
 const popup = new mapboxgl.Popup({ closeButton: true });
 
-axios.get('/rawdata')
-	.then(response => {
-		let coordinates = response.data
+mapboxgl.accessToken = "pk.eyJ1IjoianVsaWFuYS1jYXJ1c28iLCJhIjoiY2tocWJvaGc0MHMwbDJ0cDU2cWJtd2NyNyJ9.IOr-Kg5Er1_7jZeLEIK0-A";
 
-		// console.log(response.data)
 
-		coordinates.forEach((location) => {
-			// console.log(location);
-			// https://docs.mapbox.com/mapbox-gl-js/api/markers/
-			let marker = new mapboxgl.Marker({
-				scale: 1,
-				draggable: false,
-				color: "red",
-				rotation: 10,
-			});
-			marker.setLngLat(location);
-			marker.addTo(map);
-		});
-			// first do the console.log and then do the rest of the stuff
-			// marker.on("dragend", (data) => {
-			// 	// console.log("hello? Is it data?", data.target.getLngLat());
-			// 	popup.addTo(map);
-			// 	popup.setLngLat(data.target.getLngLat());
-			// 	popup.setMaxWidth("400px");
-			// 	popup.setHTML(
-			// 		`<h2>What a location</h2> <h3>location: ${data.target.getLngLat()} </h3>`
-			// );
+if (window.location.pathname === '/') {
+	map = new mapboxgl.Map({
+	  container: "map",
+	  style: 'mapbox://styles/mapbox/streets-v11',
+	  center: [13.4050, 52.5200],
+	  zoom: 9,
+	  doubleClickZoom: true,
+	  pitch: 30,
 	});
-			
-// });
+	axios.get('/rawdata')
+		.then(response => {
+			let coordinates = response.data
 
-mapboxgl.accessToken =
-"pk.eyJ1IjoianVsaWFuYS1jYXJ1c28iLCJhIjoiY2tocWJvaGc0MHMwbDJ0cDU2cWJtd2NyNyJ9.IOr-Kg5Er1_7jZeLEIK0-A";
-const map = new mapboxgl.Map({
-    container: "map",
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [13.4050, 52.5200],
-    zoom: 11,
-    doubleClickZoom: true,
-    pitch: 0,
-});
+			// console.log(response.data)
+
+			coordinates.forEach((location) => {
+				// console.log(location);
+				// https://docs.mapbox.com/mapbox-gl-js/api/markers/
+				let marker = new mapboxgl.Marker({
+					scale: 1,
+					draggable: false,
+					color: "red",
+					rotation: 10,
+				});
+				marker.setLngLat(location);
+				marker.addTo(map);
+			});
+		});
+				
+		} else if (window.location.pathname === '/search') {
+			map = new mapboxgl.Map({
+			container: "map",
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [13.4050, 52.5200],
+			zoom: 9,
+			doubleClickZoom: true,
+			pitch: 30,
+			});
+		}
+  
 
 let userMarker = new mapboxgl.Marker({
 	scale: 1,
@@ -78,15 +81,3 @@ userMarker.on("dragend", (data) => {
 		});
 	});
 });
-
-
-
-
-// demo adding a pop up
-// const popup = new mapboxgl.Popup({ closeButton: true });
-// popup.addTo(map);
-// popup.setLngLat([13.405, 52.52]);
-// popup.setMaxWidth("400px");
-// popup.setHTML(
-	// `<h2>Click here for more info!</h2> <button>see the world</button>`
-// );

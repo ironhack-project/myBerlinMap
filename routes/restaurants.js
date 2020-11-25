7804ee3664b3673a6e7d5834238a95c9588ba2f3
Kara P <quetzlcoatlus@gmail.com>
2020-11-25 14:25:59 +0100

    search through page but not in api displayed items

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -11,5 +11,37 @@ router.get('/all' , (req,res) => {
        });
 });
 
+// app.get('/searchResults/restaurant/:text', (req,res) => { 
+//   const restaurant = req.query.restaurant.toLowerCase()
+//   .searchArtists(artist)
+//   .then(data => {
+//       console.log('artists: ', data.body.artists.items);
+//       // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+//       res.render('details', {})
+//   })   
+//   .catch(err => console.log('The error while searching the restaurants occurred: ', err));
+// })
+
+
+
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+
 
 module.exports = router;

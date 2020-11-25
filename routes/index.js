a58caf981e4c8424ca28691be0c0f1097954c5d8
Agustina Garro <agustinagarro97@gmail.com>
2020-11-25 09:35:48 +0100

    login signup privatepage

--- routes/index.js
+++ routes/index.js
@@ -44,13 +44,35 @@ router.get('/rawdatarestaurantNames', (req,res,next) => {
   return res.json (restaurantNames)
 });
 
-router.get('/search', (req,res,next) => {
+router.get('/search', (req,res, next) => {
   res.render ('search')
   axios.get('https://api.quandoo.com/v1/merchants?place=Berlin&radius=10&capacity=2&offset=0&limit=10000')
     .then(response => {
       const restaurantList = response.data.merchants;
       res.render('search', { restaurantList })
     });
+});
+
+router.get ('/' , (req,res,next) => {
+  const loggedinUser = req.session.user;
+  console.log({ loggedinUser });
+  res.render('index', { user: loggedinUser });
+});
+
+const loginCheck = () => {
+  return (req, res, next) => {
+    // if the user is logged in we proceed as intended (call next())
+    if (req.session.user) {
+      next();
+    } else {
+      // if user is not logged in we redirect to login
+      res.redirect('/login');
+    }
+  }
+}
+
+router.get('/private', loginCheck(), (req, res, next) => {
+  res.render('private');
 });
 
 

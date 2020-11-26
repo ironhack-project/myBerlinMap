fefa3d26eb1cb68542be968f7409dcd7ba5a58af
Kara P <quetzlcoatlus@gmail.com>
2020-11-26 18:28:59 +0100

    post merge fixing

--- init-app.js
+++ init-app.js
@@ -59,58 +59,14 @@ async function initApp({ mongooseConnection }) {
 
     const restaurants = require('./routes/restaurants');
     app.use('/', restaurants)
+
+    const mylist = require('./routes/mylist');
+    app.use('/', mylist)
+
     return app;
+  
 }
 
-//==========================================================================
-
-//==========================================================================
-// mongoose
-//   .connect(process.env.MONGODB_URI, {
-//     useCreateIndex: true,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//   .then(x => {
-//     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-//   })
-//   .catch(err => {
-//     console.error('Error connecting to mongo', err)
-//   });
-
-// const app_name = require('./package.json').name;
-// const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-
-// const app = express();
-
-// // Middleware Setup
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-
-// // session configuration
-// const session = require('express-session');
-// const MongoStore = require('connect-mongo')(session);
-
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     cookie: { maxAge: 24 * 60 * 60 * 1000 },
-//     saveUninitialized: false,
-//     resave: true,
-//     store: new MongoStore({
-//       // when the session cookie has an expiration date
-//       // connect-mongo will use it, otherwise it will create a new 
-//       // one and use ttl - time to live - in that case one day
-//       mongooseConnection: mongoose.connection,
-//       ttl: 24 * 60 * 60 * 1000
-//     })
-//   })
-// )
-//====================================================================================
-
-// Express View engine setup
 
 module.exports = {
     initApp,

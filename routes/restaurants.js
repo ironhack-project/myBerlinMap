27b7886cf47fbc9351a93a11b7abf9fa307ceb35
Kara P <quetzlcoatlus@gmail.com>
2020-11-27 09:33:19 +0100

    undid details login check

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -3,15 +3,15 @@ const router  = express.Router();
 const axios = require('axios');
 
 
-const loginCheck = () => {
-  return (req, res, next) => {
-    if (req.session.user) {
-      next();
-    } else {
-      res.redirect('/login');
-    }
-  }
-}
+// const loginCheck = () => {
+//   return (req, res, next) => {
+//     if (req.session.user) {
+//       next();
+//     } else {
+//       res.redirect('/login');
+//     }
+//   }
+// }
 
 router.get('/all' , (req,res) => {
   const loggedinUser = req.session.user;
@@ -19,11 +19,11 @@ router.get('/all' , (req,res) => {
       .then(response => {
         const restaurantList = response.data.merchants;
         // console.log(restaurantList)
-        if (req.session.user) {
+        // if (req.session.user) {
+        //   res.render('./restaurants/all', {restaurantList , user : loggedinUser })
+        // } else {
           res.render('./restaurants/all', {restaurantList , user : loggedinUser })
-        } else {
-          res.render('./restaurants/all', {restaurantList , user : loggedinUser })
-      }
+      // }
         // res.render('./restaurants/all', {restaurantList});
       });
 });
